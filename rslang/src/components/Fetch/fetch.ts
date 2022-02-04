ec8835e80cc223c780b8d1e9eadc328ad585b5c8
Andrei Skrinnik <rakyt4gin@gmail.com>
2022-02-04 20:01:48 +0300

    feat: implement "get words" fetch interface

---
+++ rslang/src/components/Fetch/fetch.ts
@@ -0,0 +1,30 @@
+interface IData {
+  url: string,
+  method: string,
+  body?: null
+}
+
+class Fetch {
+
+  async GET_WORDS<T>(group: string, page: string): Promise<T> {
+    const data: IData = {
+      url: `words?group=${group}&page=${page}`,
+      method: 'GET'
+    }
+    return await this.sendRequest(data)
+  }
+
+ 
+  async sendRequest<T>(data: IData): Promise<T> {
+    const response = await fetch(`https://rss21q3-rslang.herokuapp.com/${data.url}`, {
+      method: data.method,
+      headers: {
+        'Content-Type': 'text/plain;charset=UTF-8'
+      },
+      body: data.body
+    })
+    return await response.json()
+  }
+}
+
+export { Fetch }
