5186a63b0aefe9ecb374a0669b75369c37a97ea1
AvlasenkoPetr <phone.petr@gmail.com>
2022-02-07 13:19:36 +0300

    feat: set navigation on all window

--- rslang/src/components/pages/login-page/login-page.ts
+++ rslang/src/components/pages/login-page/login-page.ts
@@ -1,3 +1,4 @@
+import { appendFooter } from '../../footer/footer'
 import './login-page.scss'
 
 export class LoginPage {
@@ -13,6 +14,23 @@ export class LoginPage {
 
         const loginPage: HTMLElement = document.createElement('main')
         loginPage.className = 'login-page'
+        loginPage.innerHTML = `
+        <div class="login-page__title">
+        <p>Добро пожаловать в</p>
+        <h1><span>RS</span>Lang</h1>
+        <p>лучшее место для изучения английского языка!</p>
+        </div>
 
+        <input type="email" placeholder="Введите e-mail">
+
+        <input type="password" placeholder="Введите пароль">
+
+        <button class="login-page__button">ВОЙТИ</button>
+        <p>Еще нет аккаунта? Самое время <a>создать</a>!</p>
+        <button class="login-page__button light">Продолжить как гость</button>
+        `
+        // вешаем листенеры на логин пейдж
+        this.MAIN_WRAPPER.append(loginPage)
+        appendFooter(this.MAIN_WRAPPER)
     }
 }
--- rslang/src/components/router/router.ts
+++ rslang/src/components/router/router.ts
@@ -2,42 +2,49 @@ import { LoginPage } from '../pages/login-page/login-page'
 import './router.scss'
 
 export class Router {
-  NAV_BLOCK: HTMLElement | null
+  NAV_BLOCK: HTMLElement
 
   constructor() {
-    this.NAV_BLOCK = document.querySelector('.aside')
+    this.NAV_BLOCK = document.querySelector('.aside') as HTMLElement
   }
 
   setListener(): void {
-    if (!this.NAV_BLOCK) return
-
-    this.NAV_BLOCK.addEventListener('click', this.renderPage)
+    window.addEventListener('click', this.processClick)
   }
 
-  renderPage = (e: MouseEvent): void => {
+  processClick = (e: MouseEvent): void => {
     if (!(e.target instanceof HTMLElement)) return
-    if (!this.NAV_BLOCK) return
-
+    
     const clickedButton: HTMLElement = e.target
+    if (!clickedButton.dataset.navigation) return
 
-    if (clickedButton.dataset.navigation === 'open') {
-      this.NAV_BLOCK.classList.toggle('opened')
+    const clickedDataset: string = clickedButton.dataset.navigation
+
+    if (clickedDataset === 'open') {
+      this.openMenu()
       return
     }
 
+    // вынести в отдельную ф-цию (снимаем класс эктив в нав блоке)
     const currentlyActiveButton: HTMLElement | null = this.NAV_BLOCK.querySelector('.active')
     if (currentlyActiveButton) {
-      if (currentlyActiveButton.dataset.navigation === e.target.dataset.navigation) return
+      if (currentlyActiveButton.dataset.navigation === clickedDataset) return
       currentlyActiveButton.classList.remove('active')
     }
     
-    clickedButton.classList.add('active')
+    // вынести в отдельную ф-цию (разбираемся был ли клик в нав блоке и вешаем класс эктив на нужную кнопку)
+    if (clickedButton.closest('aside')) {
+      clickedButton.classList.add('active')
+    } else {
+      const navClickedButton: HTMLElement | null = this.NAV_BLOCK.querySelector(`[data-navigation=${clickedDataset}]`)
+      navClickedButton?.classList.add('active')
+    }
 
-    // Работает через дата-атрибуты детей эл-та aside
+    // Работает через дата-атрибуты
     // прим.: кнопка для переклчения на страницу книги имеет атрибут data-navigation='book', т.е.
     // пишем: case 'book': ${метод рендера у класса страницы книги} return
 
-    switch (clickedButton.dataset.navigation) {
+    switch (clickedDataset) {
       case 'main':  // ф-ция рендера
         return
 
@@ -58,5 +65,9 @@ export class Router {
         loginPage.render()
         return
     }
+  }
+
+  openMenu = (): void => {
+    this.NAV_BLOCK.classList.toggle('opened')
   }
 }
