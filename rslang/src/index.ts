049c0ca4c6d858edc3399e79f949a7d7028ecc4b
AvlasenkoPetr <phone.petr@gmail.com>
2022-02-11 20:20:10 +0300

    refactor: remove login save to login-page.ts

--- rslang/src/index.ts
+++ rslang/src/index.ts
@@ -4,10 +4,3 @@ import { Router } from './components/router/router'
 
 const router = new Router
 router.setListener()
-
-// временная затычка для отрисовки залогиненного юзера
-window.addEventListener('load', () => {
-  if (localStorage.getItem('UserInfo')) {
-    (document.querySelector('[data-navigation="login"]') as HTMLElement).dataset.navigation = 'logout'
-  }
-})
