522139374ea68b1ac391463bb070b8fae1cf9e53
AvlasenkoPetr <phone.petr@gmail.com>
2022-02-22 02:40:33 +0300

    feat: add refreshToken handling

--- rslang/src/index.ts
+++ rslang/src/index.ts
@@ -1,6 +1,6 @@
 import './assets/styles/default.scss'
 import './assets/styles/alternative.scss'
-import { checkStatsDay, getLastPage, isUserExists, renderBookWithLS } from './components/Helpers/helpers';
+import { checkStatsDay, getLastPage, isTokenAlive, isUserExists, renderBookWithLS } from './components/Helpers/helpers';
 import { BookPage } from './components/pages/book-page/book-page';
 import './components/pages/main-page/main-page.scss'
 import { Router } from './components/router/router'
@@ -13,4 +13,7 @@ getLastPage();
 
 if (isUserExists()) {
   checkStatsDay()
+  isTokenAlive()
 }
+
+console.log('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMDJhNTY5ZmJhZDk3MDAxNmQ3YzVmOCIsImlhdCI6MTY0NTQ4NjI4NCwiZXhwIjoxNjQ1NTAwNjg0fQ.DhNfqQiksvWDttmmlg03W8wL35s4VGApBv4hkwdwYGI');
